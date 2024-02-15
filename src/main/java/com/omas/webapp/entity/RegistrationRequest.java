f92cb6e8c56e6f63ff6a37cd879b6df274601718
Lauri.L <late4460@gmail.com>
2024-02-15 07:59:38 +0200

    create RegistrationRequest for user controller

---
+++ src/main/java/com/omas/webapp/entity/RegistrationRequest.java
@@ -0,0 +1,37 @@
+package com.omas.webapp.entity;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor; 
+import lombok.Data; 
+import lombok.NoArgsConstructor; 
+  
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegistrationRequest { 
+    
+	
+	@NotEmpty(message = "Username cannot be empty")
+	@Size(max = 64,  message = "Username cannot be larger than 64 characters")
+	@Size(min = 3, message = "Username cannot be fewer than 3.")
+	private String username; 
+
+    	
+	@NotEmpty(message = "name cannot be empty")
+	@Size(max = 64,  message = "Username cannot be larger than 64 characters")
+	@Size(min = 3, message = "name cannot be fewer than 3.")
+	private String name; 
+
+	@NotEmpty(message = "Email cannot be empty.")
+	@Email(message = "Email should be in the correct format.")
+	private String email;
+    
+	@NotEmpty(message = "Password cannot be empty.")
+    @Size(max = 64,  message = "Username cannot be larger than 64 characters")
+	@Size(min = 6, message = "Password cannot be fewer than 6 characters")
+	private String password;
+
+  
+}
