cb02307cdf8070aa4243725b04f5f507927ef117
Lauri.L <late4460@gmail.com>
2024-03-16 06:02:21 +0200

    update token generation to be more secure

--- src/main/java/com/omas/webapp/controller/UserController.java
+++ src/main/java/com/omas/webapp/controller/UserController.java
@@ -1,9 +1,12 @@
 package com.omas.webapp.controller;
 
+import java.math.BigInteger;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.security.SecureRandom;
 import java.util.HashMap;
 import java.util.Map;
 import com.omas.webapp.entity.response.MessageResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
@@ -28,7 +31,6 @@ import com.omas.webapp.service.UserService;
 import com.omas.webapp.table.User;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
-import net.bytebuddy.utility.RandomString;
 
 @RestController
 @RequestMapping("/api")
@@ -47,7 +49,7 @@ public class UserController {
     private MailService mailService;
 
     @Value("${frontend.RecoveryPage}")
-	private String recoveryPage; 
+    private String recoveryPage;
 
     @PostMapping("/reg")
     public ResponseEntity<?> addNewUser(@Valid @RequestBody RegistrationRequest request) {
@@ -95,11 +97,14 @@ public class UserController {
     public ResponseEntity<?> processForgotPassword(@Valid @RequestBody PasswordRecoveryRequest reqRequest) {
 
         String email = reqRequest.getEmail();
-        String token = RandomString.make(30);
+
+        SecureRandom random = new SecureRandom();
+        String token = new BigInteger(130, random).toString(32);
+        token = URLEncoder.encode(token, StandardCharsets.UTF_8);
 
         try {
             service.updateResetPasswordToken(token, email);
-            String resetPasswordLink = recoveryPage +"?token="+ token;
+            String resetPasswordLink = recoveryPage + "?token=" + token;
             mailService.sendRecoveryEmail(email, resetPasswordLink);
         } catch (Exception e) {
             return new ResponseEntity<>(Map.of("message", "email not sent, " + e.getMessage()),
@@ -108,21 +113,19 @@ public class UserController {
         return new ResponseEntity<>(Map.of("message", "email sent"), HttpStatus.OK);
     }
 
-
-
     @PostMapping("/reset_password")
     public ResponseEntity<?> processResetPassword(HttpServletRequest request, PasswordResetRequest resetRequest) {
 
         User user = service.getByResetPasswordToken(resetRequest.getToken());
 
         if (user == null) {
-            return new ResponseEntity<>(Map.of("message", "Token is either invalid or expired"), HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(Map.of("message", "Token is either invalid or expired"),
+                    HttpStatus.BAD_REQUEST);
         } else {
             service.updatePassword(user, resetRequest.getPassword());
             return new ResponseEntity<>(Map.of("message", "password updated"), HttpStatus.OK);
         }
     }
-
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
