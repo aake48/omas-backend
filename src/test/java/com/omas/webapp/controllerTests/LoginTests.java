9c11233c96e057c394973fef679ac928b55f42a9
Lauri.L <late4460@gmail.com>
2024-03-21 00:02:29 +0200

    remove unnecessary logins

--- src/test/java/com/omas/webapp/controllerTests/LoginTests.java
+++ src/test/java/com/omas/webapp/controllerTests/LoginTests.java
@@ -27,34 +27,22 @@ public class LoginTests {
 
         private static final String url = "/api/login";
 
-        private String json;
-
         private String username = "johndoe";
 
         @BeforeEach
         private void registerUser() throws Exception {
 
-                /*  
-                .put("password", "password123")
-                .put("username", username)
-                .put("name", "name")
-                .put("email", "email@"+username+".com") 
-                */
                 TestUtils.getToken(mockMvc, "johndoe");
-
-                json = new JSONObject()
-                .put("password", "password123")
-                .put("username", username)
-                .toString();
-
-                // json = new JSONObject()
-                // .put("email", "email@johndoe.com")
-                // .put("password", "password123")
-                // .toString();
         }
 
         @Test
         void login() throws Exception {
+
+                String json = new JSONObject()
+                                .put("password", "password123")
+                                .put("username", username)
+                                .toString();
+
                 mockMvc.perform(MockMvcRequestBuilders.post(url)
                                 .contentType(MediaType.APPLICATION_JSON)
                                 .content(json))
@@ -73,20 +61,20 @@ public class LoginTests {
 
         @Test
         void registerAndDeleteUser() throws Exception {
-                
+
                 String deletionUrl = "/api/delete";
 
                 String adminToken = TestUtils.loginAdmin(mockMvc);
 
                 String deletionResponse = mockMvc.perform(MockMvcRequestBuilders.delete(deletionUrl)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .header("Authorization", "Bearer " + adminToken)
-                        .content(username))
-                        .andExpect(jsonPath("$.message").value("User deleted"))
-                        .andExpect(status().isOk())
-                        .andReturn()
-                        .getResponse()
-                        .getContentAsString();
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .header("Authorization", "Bearer " + adminToken)
+                                .content(username))
+                                .andExpect(jsonPath("$.message").value("User deleted"))
+                                .andExpect(status().isOk())
+                                .andReturn()
+                                .getResponse()
+                                .getContentAsString();
 
                 System.out.println("deletionResponse: " + deletionResponse);
         }
@@ -95,8 +83,6 @@ public class LoginTests {
         void IncorrectCapitalizedLetterInPassword() throws Exception {
 
                 TestUtils.getToken(mockMvc, "johndoe");
-
-                login();
 
                 // Incorrectly Capitalized first letter of password
                 mockMvc.perform(MockMvcRequestBuilders.post(url)
@@ -115,8 +101,6 @@ public class LoginTests {
 
                 TestUtils.getToken(mockMvc, "johndoe");
 
-                login();
-
                 // Incorrect whitespace before password
                 mockMvc.perform(MockMvcRequestBuilders.post(url)
                                 .contentType(MediaType.APPLICATION_JSON)
@@ -134,8 +118,6 @@ public class LoginTests {
 
                 TestUtils.getToken(mockMvc, "johndoe");
 
-                login();
-
                 // Incorrect whitespace in Username
                 mockMvc.perform(MockMvcRequestBuilders.post(url)
                                 .contentType(MediaType.APPLICATION_JSON)
@@ -151,8 +133,6 @@ public class LoginTests {
         void IncorrectCapitalizedLetterInUsername() throws Exception {
 
                 TestUtils.getToken(mockMvc, "johndoe");
-
-                login();
 
                 // Incorrectly Capitalized Username
                 mockMvc.perform(MockMvcRequestBuilders.post(url)
@@ -170,8 +150,6 @@ public class LoginTests {
 
                 TestUtils.getToken(mockMvc, "johndoe");
 
-                login();
-
                 // missingPassword
                 mockMvc.perform(MockMvcRequestBuilders.post(url)
                                 .contentType(MediaType.APPLICATION_JSON)
@@ -186,8 +164,6 @@ public class LoginTests {
         void missingUsername() throws Exception {
 
                 TestUtils.getToken(mockMvc, "johndoe");
-
-                login();
 
                 // missingUsername
                 mockMvc.perform(MockMvcRequestBuilders.post(url)
