7b6568e7a704d656ce2ac3fd073a4aa2bd023217
TotalledZebra <Holappa57@gmail.com>
2024-03-23 01:27:41 +0200

    (WIP) image posting

---
+++ src/main/java/com/omas/webapp/controller/FileController.java
@@ -0,0 +1,75 @@
+package com.omas.webapp.controller;
+
+import com.omas.webapp.entity.response.MessageResponse;
+import com.omas.webapp.service.FileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.FileNotFoundException;
+import java.nio.file.FileAlreadyExistsException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/file")
+public class FileController {
+
+    @Autowired
+    private FileService service;
+
+    public static final int MEGABYTE = 1_000_000;
+    public static final int MAX_FILE_SIZE = 8 * MEGABYTE;
+
+    @PreAuthorize("hasAuthority('ROLE_USER')")
+    @PostMapping("/upload")
+    public ResponseEntity<?> uploadFiles(
+        @RequestParam("competitionId") String competitionId,
+        @RequestParam("file") MultipartFile file
+    ) {
+
+        // Most likely will be removed later once I know how Spring configurations fully work
+        // Namely the following in application.properties:
+        // spring.servlet.multipart.max-file-size=10MB
+        // spring.servlet.multipart.max-request-size=10MB
+        // server.tomcat.max-swallow-size=100MB
+        if (file.getSize() > MAX_FILE_SIZE) {
+            return new MessageResponse("The image you tried to upload is too large", HttpStatus.BAD_REQUEST);
+        }
+
+        try {
+            // This naming may be confusing but the idea is that here the server is downloading the image uploaded by the user
+            service.receiveAndWriteFileFully(competitionId, file);
+        } catch (FileAlreadyExistsException ex) {
+            return new MessageResponse("Could not upload an image with that file name: The file already exists.", HttpStatus.BAD_REQUEST);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return new MessageResponse("Something went wrong with uploading the image: " + ex.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+
+        return new MessageResponse("File uploaded successfully", HttpStatus.OK);
+    }
+
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    @GetMapping("/download/{fileName}")
+    public ResponseEntity<?> downloadFile(@PathVariable String fileName) {
+
+        try {
+            return new ResponseEntity<>(service.getFile(fileName), HttpStatus.OK);
+        } catch (FileNotFoundException ex) {
+            return new MessageResponse("The requested file could not be found", HttpStatus.BAD_REQUEST);
+        } catch (Exception ex) {
+            return new MessageResponse("Downloading the file failed: " + ex.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+
+    }
+
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    @GetMapping("/files")
+    public List<String> getFileNames() {
+        return service.getFiles();
+    }
+
+}
---
+++ src/main/java/com/omas/webapp/service/FileService.java
@@ -0,0 +1,134 @@
+package com.omas.webapp.service;
+
+import com.omas.webapp.config.FileStorageProperties;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
+
+@Service
+public class FileService {
+
+    // SimpleDateFormat is not thread-safe
+    private final ThreadLocal<SimpleDateFormat> dateFormat = ThreadLocal.withInitial(
+        () -> new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss")
+    );
+
+    private final Path fileDirectory;
+
+    public FileService(FileStorageProperties properties) throws IOException {
+        this.fileDirectory = Paths.get(properties.getPath()).toAbsolutePath().normalize();
+        Files.createDirectories(this.fileDirectory);
+    }
+
+    /**
+     * Receive and write an image to the image folder with the given file name
+     * @throws IOException if something goes wrong while processing the image
+     */
+    public void receiveAndWriteFileFully(String competitionId, MultipartFile file) throws IOException {
+
+        String userId = String.valueOf(UserInfoDetails.getDetails().getId());
+        String timeStamp = this.dateFormat.get().format(new Date(System.currentTimeMillis()));
+        String fileExtension = extractFileExtension(file);
+
+        Path filePath = resolveFilePath(competitionId, userId, timeStamp, fileExtension);
+
+        Files.copy(file.getInputStream(), filePath);
+    }
+
+    /**
+     * Extract a file extension from the given {@link MultipartFile}
+     * @return the extracted file extension
+     */
+    private String extractFileExtension(MultipartFile file) {
+
+        // Assume whatever file extension to be the default and then attempt to parse it from the provided file name
+        // TODO: See if it would be a better idea to just have no file extension when there is none
+        String fileExtension = ".jpg";
+
+        String originalName = file.getOriginalFilename();
+
+        if (originalName != null) {
+
+            int index = originalName.lastIndexOf('.');
+
+            if (index > 0) {
+                fileExtension = originalName.substring(index);
+            }
+        }
+
+        return fileExtension;
+    }
+
+    /**
+     * Build a file path out of the given information to reduce file name collisions
+     * @return the built path
+     */
+    private Path resolveFilePath(String competitionId, String userId, String timeStamp, String fileExtension) {
+
+        String fileName = String.join("_", competitionId, userId, timeStamp, fileExtension);
+
+        Path path = this.fileDirectory.resolve(fileName);
+
+        int number = 1;
+
+        // This almost feels like overengineering
+        while (path.toFile().exists()) {
+            fileName = String.join("_", competitionId, userId, timeStamp, String.valueOf(number), fileExtension);
+            path = this.fileDirectory.resolve(fileName);
+        }
+
+        return path;
+    }
+
+    /**
+     * Get the image from the image folder
+     * @param fileName the name of the file
+     * @return the image bytes to be sent to the client
+     * @throws IOException if getting
+     */
+    public Resource getFile(String fileName) throws IOException {
+
+        Path filePath = this.fileDirectory.resolve(fileName);
+
+        if (!Files.exists(filePath)) {
+            throw new FileNotFoundException("File named " + fileName + " could not be found");
+        }
+
+        return new ByteArrayResource(Files.readAllBytes(filePath));
+    }
+
+    /**
+     * Get all the files in the directory
+     * @return the files
+     */
+    public List<String> getFiles() {
+
+        List<String> files = new ArrayList<>();
+
+        // The Objects.requireNonNull is not 100% necessary here but
+        // the editor complains about the potential NPE even though the directory will only be null
+        // if it is not configured. This entire class will fail much earlier if that happens
+        for (File file : Objects.requireNonNull(this.fileDirectory.toFile().listFiles())) {
+
+            if (file.isFile()) {
+                files.add(file.getName());
+            }
+        }
+
+        return files;
+    }
+
+}
