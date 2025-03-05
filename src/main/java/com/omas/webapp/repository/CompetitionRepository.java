36cf61ad1bbc40d240251dfcb5b8beb8d0b64b3f
Aleksi Santala <asantala22@student.oulu.fi>
2025-03-05 20:20:54 +0200

    filter competitions by series

--- src/main/java/com/omas/webapp/repository/CompetitionRepository.java
+++ src/main/java/com/omas/webapp/repository/CompetitionRepository.java
@@ -13,6 +13,12 @@ public interface CompetitionRepository extends JpaRepository<Competition, String
 
     Page<Competition> findByCompetitionIdContainingIgnoreCase(String name, PageRequest pageRequest);
 
+    @Query(value = "SELECT * FROM Competition c WHERE c.competition_series = :series", nativeQuery = true)
+    Page<Competition> findBySeries(@Param("series") String series, PageRequest pageRequest);
+
+    @Query(value = "SELECT * FROM Competition c WHERE c.competition_series = :series AND LOWER(c.competition_id) iLIKE %:search%", nativeQuery = true)
+    Page<Competition> findBySeriesContaining(@Param("series") String series, @Param("search") String search, PageRequest pageRequest);
+
     @Query(value = "SELECT * FROM Competition c WHERE EXTRACT(YEAR FROM c.start_date) = :year ORDER BY c.start_date ASC", nativeQuery = true)
     Page<Competition> findByYear(@Param("year") int year, PageRequest pageRequest);
 
